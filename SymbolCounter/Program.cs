33b02a566a370b79549b4eca4ea3e39ef2149218
NataliaZhydeikina <nata.iiinx88@gmail.com>
2021-08-30 07:49:48 +0300

    add SymbolCounter project

---
+++ SymbolCounter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SymbolCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteResult(SymbolCount(ReadString()));
+        }
+
+        static string ReadString()
+        {
+            Console.WriteLine("Please enter string:");
+            return Console.ReadLine();
+        }
+
+        static int SymbolCount(string str)
+        {
+            str = str.Replace(" ", "");
+            return str.Length;
+        }
+        
+        static void WriteResult(int count)
+        {
+            Console.WriteLine($"String contains {count} symbols");
+        }
+    }
+}
